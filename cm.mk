9745e60723280d8d79679e052a557b91e8b4f0cc
mrimp <mrimp@day-off.net>
2013-12-27 18:08:09 -0500

    KK update

--- cm.mk
+++ cm.mk
@@ -9,11 +9,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevl TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/jfltevl/jfltecan:4.2.2/JDQ39/I337MVLUAMDJ:user/release-keys" PRIVATE_BUILD_DESC="jfltevl-user 4.2.2 JDQ39 I337MVLUAMDJ release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecan TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/oct_jfltecan/jfltecan:4.4/KRT16S/user/release-keys" PRIVATE_BUILD_DESC="jfltecan-user 4.4 KRT16S release-keys"
 
 PRODUCT_NAME := cm_jfltecan
 PRODUCT_DEVICE := jfltecan
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SGH-I337M
-PRODUCT_MANUFACTURER := Samsung
 
