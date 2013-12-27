cdbc71714acf4b7e07e6d8b04288c4656d5f24d5
mrimp <mrimp@day-off.net>
2013-12-27 18:14:27 -0500

    updated release keys

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecan TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/oct_jfltecan/jfltecan:4.4/KRT16S/user/release-keys" PRIVATE_BUILD_DESC="jfltecan-user 4.4 KRT16S release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecan TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/jfltecan/jfltecan:4.2.2/JDQ39/i337UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 i337UVUAMDB release-keys"
 
 PRODUCT_NAME := cm_jfltecan
 PRODUCT_DEVICE := jfltecan
